838e58157c5c0487143760778c6930c89c35c114
AvinashKumar3000 <48342929+AvinashKumar3000@users.noreply.github.com>
2021-01-07 18:11:50 +0530

    Update app.js
    An API key is removed for security purposes.. as the project is public.

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDz6WnKHACvvsUVlrQD_LIa_TZq_bd-zRQ",
-    authDomain: "time-table-58e18.firebaseapp.com",
-    databaseURL: "https://time-table-58e18.firebaseio.com",
-    projectId: "time-table-58e18",
-    storageBucket: "time-table-58e18.appspot.com",
-    messagingSenderId: "429614196004",
-    appId: "1:429614196004:web:802198221cec632a987fb1",
-    measurementId: "G-6FKBV2DRJ0"
+    apiKey: "API KEY",
+    authDomain: "AUTH DOMAIN",
+    databaseURL: "DATABASE URL",
+    projectId: "PROJECT ID",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
